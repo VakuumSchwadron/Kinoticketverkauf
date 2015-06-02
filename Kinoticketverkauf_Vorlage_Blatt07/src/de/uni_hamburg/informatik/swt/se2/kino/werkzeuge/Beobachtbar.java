93bb5d5c8e4315c5e430a6420f105a820bcedfea
jannik <jannik@PC-Wyrnat>
2015-06-02 14:35:14 +0200

    Versuch2: Beobachtbar fertig

---
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachtbar.java
@@ -0,0 +1,71 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Diese Klasse liefert die Funktionalität für beobachtbare Services. Definiert Operationen zum An- und
+ * Abmelden von Beobachtern.
+ * 
+ * @author VakuumSchwadron
+ * @version SoSe 2015
+ */
+public abstract class Beobachtbar
+{
+
+    /**
+     * Die Liste der registrierten Beobachter.
+     */
+    private List<Beobachter> _beobachter;
+
+    /**
+     * Initialisiert die Klasse Beobachtbar.
+     */
+    public Beobachtbar()
+    {
+        _beobachter = new ArrayList<Beobachter>();
+    }
+    
+    
+    /**
+     * Meldet den gegebenen Beoabachter beim an.
+     * 
+     * @param beobachter Ein Beobachter, der angemeldet werden soll.
+     * 
+     * @require beobachter != null
+     */
+    public void registriereBeobachter(Beobachter beobachter)
+    {
+        assert beobachter != null : "Vorbedingung verletzt: beobachter != null";
+        if (!_beobachter.contains(beobachter))
+        {
+            _beobachter.add(beobachter);
+        }
+    }
+    
+    
+    /**
+     * Meldet den gegebenen Beoabachter beim ab.
+     * 
+     * @param beobachter Ein Beobachter, der abgemeldet werden soll.
+     * 
+     * @require beobachter != null
+     */
+    public void entferneBeobachter(Beobachter beobachter)
+    {
+        assert beobachter != null : "Vorbedingung verletzt: beobachter != null";
+        _beobachter.remove(beobachter);
+    }
+
+    /**
+     * Informiert alle angemeldeten Beobachter dass eine relevante Änderung
+     * eingetreten ist.
+     */
+    protected void informiereUeberAenderung()
+    {
+        for (Beobachter beobachter : _beobachter)
+        {
+            beobachter.reagiereAufAenderung(this);
+        }
+    }
+}
---
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/BeobachtbarTest.java
@@ -0,0 +1,36 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.fachwerte.Kundennummer;
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.Kunde;
+
+public class BeobachtbarTest
+{
+
+    private Beobachtbar _drEvil
+    private Beobachter _jamesBond;
+    private Beobachter _sherlockHomes;
+    private Beobachter _detektivConan;
+
+    public BeobachtbarTest()
+    {
+        _drEvil = new Beobachtbar();
+        _jamesBond = new Beobachter(_beobachtbar);
+    }
+
+    @Test
+    public void testFuegeBeobachterHinzu()
+    {
+    }
+    
+    
+}
