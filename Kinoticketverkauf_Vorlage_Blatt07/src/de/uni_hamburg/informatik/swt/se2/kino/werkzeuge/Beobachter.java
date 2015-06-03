b1af5d482093d90c4b6e0b7655769c83d24d9c2a
jannik <jannik@PC-Wyrnat>
2015-06-03 16:26:41 +0200

    Beobachter: Vertragsmodell und Schnittstellenkommtar eingefuegt

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachter.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachter.java
@@ -1,10 +1,21 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
 
 /**
- * Beobachter-Interface, um Änderungen registrieren zu können  
+ * Beobachter-Interface, um Änderungen registrieren zu können
+ * 
+ * @author VakuumSchwadron
+ * @version SoSe 2015
  *
  */
 public interface Beobachter
+
 {
+	/**
+	 * Reagiert auf Aenderungen der beobachteten Klasse
+	 * 
+	 * @param o Die beobachtete Klasse
+	 * 
+	 * @require (o != null)
+	 */
     public void reagiereAufAenderung(Object o);
 }
