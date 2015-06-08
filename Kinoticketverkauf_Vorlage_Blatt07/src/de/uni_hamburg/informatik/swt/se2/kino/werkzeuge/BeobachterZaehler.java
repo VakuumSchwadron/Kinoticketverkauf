3657291cb37eac207c7b833fbb77fa61a3f32a50
jannik <jannik@PC-Wyrnat>
2015-06-08 15:18:12 +0200

    Und nochmal!

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/BeobachterZaehler.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/BeobachterZaehler.java
@@ -1,15 +1,19 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
 
+/**
+ * Diese Klasse implementiert das Interface Beobachter und zählt die Aufrufe der
+ * Methode reagiereAufAenderung(Object o). 
+ * 
+ * @author VakuumSchwadron
+ * @ version SoSe15
+ *
+ */
+
 public class BeobachterZaehler implements Beobachter
 {
-	private int _zaehler;
-	
-	public BeobachterZaehler()
-	{
-		_zaehler = 0;
-		
-	}
+	private int _zaehler = 0;
 
+	@Override
 	public void reagiereAufAenderung(Object o)
 	{
 		assert o != null : "Vorbedingung verletzt: o != null";
@@ -19,6 +23,12 @@ public class BeobachterZaehler implements Beobachter
 		
 		
 	}
+	
+	/**
+	 * Gibt die Anzahl an Methodenaufrufen von reagiereAufAenderung zurück
+	 * 
+	 * @return Anzahl der Methodenaufrufe
+	 */
 	public int getZaehler()
 	{
 		return _zaehler;
--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/SubBeobachtbar.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/SubBeobachtbar.java
@@ -1,8 +1,13 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
 
+/**
+ * Eine Beobachter-Klasse für die Testklasse BeobachterTest
+ * 
+ * @author VakuumSchwadron
+ * @version SoSe15
+ *
+ */
 public class SubBeobachtbar extends Beobachtbar
 {
-	
-	
 
 }
